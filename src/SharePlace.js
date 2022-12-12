1b81e4f964bb52b6106884f05f7c075354c2466e
정원철 <gksrud316@naver.com>
2022-12-12 14:27:00 +0900

    modal class 및 show method 작성

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,6 @@
+import { Modal } from './UI/Modal'
+// 모달 모듈 추가
+
 class PlaceFinder {
     constructor() { // 생성자 함수
         const addressForm = document.querySelectorAll('form');
@@ -14,9 +17,13 @@ class PlaceFinder {
             alert("현재 Geolocation 기능을 사용할수 없습니다. 보다 최신 버전의 브라우저를 이용하거나 주소를 직접 입력하세요. ");
             return;
         }
+
+        const modal = new Modal('loading-modal-content', 'Loading location - 기다려 주세요!');
+        modal.show();
         // 현재 위치 얻기
         navigator.geolocation.getCurrentPosition(
             successResulut => {
+                modal.hide();
                 const coordinates = {
                     lat: successResulut.coords.latitude,
                     lng: successResulut.coords.longitude
@@ -24,9 +31,9 @@ class PlaceFinder {
                 console.log(coordinates);
 
             }, error => {
+                modal.hide();
                 // 위치를 찾지 못할때
                 alert("위치를 찾을수 없습니다. 제대로된 주소를 입력하세요!")
-
             });
 
 
---
+++ src/UI/Modal.js
@@ -0,0 +1,34 @@
+// 외부에서 사용할수 있도록 처리
+export class Modal {
+
+    constructor(contentId, fallbackText) {
+        this.fallbackText = fallbackText;
+        this.contentTemplateEl = document.getElementById(contentId);
+        this.modalTemplateEl = document.getElementById("modal-template");
+    }
+
+    show() {
+        if ('content' in document.createElement("template")) {
+            const modalElements = document.importNode(this.modalTemplateEl.content, true);
+            const modalElement = modalElements.querySelector('.modal');
+            const backdropElement = modalElements.querySelector('.backdrop');
+            const contentElement = document.importNode(
+                this.contentTemplateEl.content,
+                true
+            );
+
+            modalElement.appendChild(contentElement);
+            document.body.insertAdjacentElement('afterbegin', modalElement)
+            document.body.insertAdjacentElement('afterbegin', backdropElement)
+
+
+        } else {
+            // 템플릿이 지원되지 않는 상황에서 사용자에게 보여줌
+            // fall back code
+            alert(this.fallbackText);
+        }
+
+    }
+
+    hide() {}
+}
