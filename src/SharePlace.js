bc4ae1d36b1b9fd187a2c1132449cc66d91a2778
정원철 <gksrud316@naver.com>
2022-12-12 14:01:00 +0900

    현재 위치 가져오기, 예외 처리

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -4,11 +4,31 @@ class PlaceFinder {
         const locateUserBtn = document.getElementById('locate-btn');
 
         locateUserBtn.addEventListener("click", this.locateUserHandler);
-        addressForm.addEventListener("submit", this.findAddressHandler);
+        // addressForm.addEventListener("submit", this.findAddressHandler);
     }
 
 
     locateUserHandler() {
+        // 사용자 위치 얻기
+        if (!navigator.geolocation) {
+            alert("현재 Geolocation 기능을 사용할수 없습니다. 보다 최신 버전의 브라우저를 이용하거나 주소를 직접 입력하세요. ");
+            return;
+        }
+        // 현재 위치 얻기
+        navigator.geolocation.getCurrentPosition(
+            successResulut => {
+                const coordinates = {
+                    lat: successResulut.coords.latitude,
+                    lng: successResulut.coords.longitude
+                };
+                console.log(coordinates);
+
+            }, error => {
+                // 위치를 찾지 못할때
+                alert("위치를 찾을수 없습니다. 제대로된 주소를 입력하세요!")
+
+            });
+
 
     }
 
@@ -17,4 +37,7 @@ class PlaceFinder {
     }
 
 
+
 }
+
+const placeFinder = new PlaceFinder();
